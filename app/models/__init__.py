37c508b516d63b52939969a6a7bcbbf8254f3680
Hansen <guohansen0010@gmail.com>
2023-05-02 19:42:55 -0400

    DB3

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,10 @@
 from .db import db
 from .future50 import Future50
 from .independence100 import Independence100
+from .business import Business
+from .business_details import BusinessDetail
+from .states import State
+from .crimes_vs_property import CrimesVsProperty
+from .crimes_vs_person import CrimesVsPerson
+from .crimes_vs_society import CrimesVsSociety
+
